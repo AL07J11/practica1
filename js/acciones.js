5bba991825c906f248fbd597ccacc558be0b21c3
AL07J11 <steph.aloh@gmail.com>
2013-03-21 08:25:21 -0600

    1.1

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,43 @@
-// JavaScript Document
-$(document). ready(function(e) {
-	alert("El Documento Esta Listo")
+$(document).ready(function(e){
+ 	alert("El documento esta listo")
+ 
+ 
+ 
+ 	document.addEventListener("deviceready",function(){
+	 $('.btn').tap (function (){
+	 var boton= (($(this).attr('class')).split(' '))[1];
+	
+	switch(boton) 
+		
+	{
+			case'b1':
+				navigator.notification.beep(1);
+			break;
+			
+			case'b2':
+				navigator.notification.beep(1);
+			break;
+			
+			case'b3':
+				navigator.notification.beep(1);
+			break;
+			
+			case'v1':
+				navigator.notification.vibrate(500);
+			break;
+			
+			case'v2':
+				navigator.notification.vibrate(500);
+				navigator.notification.vibrate(500);
+			break;
+		}
+	 });	 
+	 $('#derecha').swiperight(function(){
+			navigator.notification.alert("Deslizo a la derecha",function(){ },"practica 1","Aceptar");
+	 }
+ );
+ },false);
 });
+
+
+	
